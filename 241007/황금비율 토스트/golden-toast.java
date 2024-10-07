b8b99a67c9ad56ae65bb383ab9b4428482bda4a7
sanghwa <77483110+idealflower-k@users.noreply.github.com>
2024-10-07 14:37:34 +0900

    Title: 황금비율 토스트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241007/황금비율 토스트/golden-toast.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static LinkedList<Character> list = new LinkedList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer token = new StringTokenizer(buffer.readLine());
+
+		int strLen = Integer.parseInt(token.nextToken());
+		int cmdCnt = Integer.parseInt(token.nextToken());
+
+		String str = buffer.readLine();
+		char[] chars = str.toCharArray();
+
+		for (char data : chars) {
+            list.add(data);
+        }
+
+		ListIterator<Character> iter = list.listIterator(list.size());
+
+		for (int i = 0; i < cmdCnt; i++) {
+			token = new StringTokenizer(buffer.readLine());
+			String cmd = token.nextToken();
+
+			if (cmd.equals("L")) {
+				left(iter);
+			} else if (cmd.equals("R")) {
+				right(iter);
+			} else if (cmd.equals("D")) {
+				delete(iter);
+			} else if (cmd.equals("P")) {
+				Character data = token.nextToken().charAt(0);
+				position(iter, data);
+			}
+		}
+
+		for (Character data : list) {
+			System.out.print(data);
+		}
+
+	}
+
+	private static void left(ListIterator<Character> iter) {
+		if (iter.hasPrevious()) {
+			iter.previous();
+		}
+	}
+
+	private static void right(ListIterator<Character> iter) {
+		if (iter.hasNext()) {
+			iter.next();
+		}
+	}
+
+	private static void delete(ListIterator<Character> iter) {
+		if (iter.hasNext()) {
+			iter.next();
+			iter.remove();
+		} else {
+			iter.remove();
+		}
+	}
+
+	private static void position(ListIterator<Character> iter, Character data) {
+		if (iter.hasNext()) {
+			iter.add(data);
+			iter.next();
+		} else {
+			iter.add(data);
+		}
+	}
+}
